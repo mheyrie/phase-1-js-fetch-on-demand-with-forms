20ca77020532cc921f78652064b294a43192d5e6
Mary <omota4fidelity@gmail.com>
2023-08-28 12:56:52 +0100

    completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form")
+
+    inputForm.addEventListener("submit", (e) =>{
+        e.preventDefault()
+        const input = document.querySelector("input#searchByID")
+        console.log(input.value)
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res => res.json())
+        .then(data =>{
+            const title = document.querySelector("section#movieDetails h4")
+            const summary = document.querySelector("section#movieDetails p")
+            
+            title.innerHTML = data.title
+            summary.innerHTML = data.summary
+            console.log(data)
+        })
+    })
+//   const inputForm = document.querySelector("form")
+//   inputForm.addEventListener("submit", (e) => {
+//     e.preventDefault()
+//     console.log(e.target.children[1].value)
+//     })
 }
 
 document.addEventListener('DOMContentLoaded', init);
